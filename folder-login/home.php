3c5c42f4e5dce4fbbed0ae0925306715903b8c27
Imada4545 <nataliabaru90@gmail.com>
2021-05-23 15:04:57 +0700

    first init

---
+++ folder-login/home.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <?php
+    if (isset($_SESSION['username'])) {
+        echo '<h3>Welcome ' . $_SESSION['name'] . ' - ' . ' Anda adalah ' . $_SESSION['role'] . '</h3>';
+        if ($_SESSION['role'] == 'admin') {
+            echo '<a href="item/index.php" class="btn-green" >Item Menu</a>';
+            echo '<a href="purchase/index.php" class="btn-blue" style="margin:20px;">Purchase Menu</a>';
+            echo '<a href="up" class="btn-green" >Upload</a>' ;
+
+        } else if ($_SESSION['role'] == 'user') {
+            echo '<a href="sales/index.php" class="btn-green" style="margin-right:20px;">Sales Menu</a>';
+        }
+    ?>
+    <th>
+        <a href="auth/logout.php" class="btn-red" onclick="return confirm('Are you sure')"> Logout</a>
+    </th>
+    <?php
+    } else {
+        echo '<h2>Tolong login dulu</h2>';
+        echo '<a href="auth/login.php" class="btn-blue">Login</a>';
+    }
+    ?>
+</body>
+
+</html>
