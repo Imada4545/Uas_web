04ea005ca8b94d2bd4e01f3f685775bd3b1a76aa
Sheva Athalla <shevanejiwolop@gmail.com>
2021-05-25 10:33:08 +0700

    ngubah posisi connection.php

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "website_imada";
+$dbname = "uas";
 
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
